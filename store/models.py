89f753989d2c2071f27e56f4ad40ce7a38bcd011
Mohsin Ali <mohsinaliasdf8@gmail.com>
2026-09-25 10:40:16 +0500

    Implement cart functionality and product variations

--- store/models.py
+++ store/models.py
@@ -18,3 +18,20 @@ class Product(models.Model):
         return self.product_name
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])  
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+class Variation(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category=models.CharField(max_length=100, choices=(('color','color'),('size','size')), default='color')
+    variation_value=models.CharField(max_length=100)
+    is_active=models.BooleanField(default=True)
+    created_date=models.DateTimeField(auto_now=True)
+    objects=VariationManager()
+    def __str__(self):
+        return self.variation_value
+
+
